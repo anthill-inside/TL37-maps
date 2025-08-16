36ef284fdb469d86fb68bb1dcfc401366ac8fc33
Vicky <ohmnomnom90@gmail.com>
2025-08-16 14:20:50 +0300

    remake

--- src/AnagramAppl.java
+++ src/AnagramAppl.java
@@ -35,14 +35,11 @@ public class AnagramAppl {
 		for (Character c : w1.toCharArray()) {
 			w1Frequencies.put(c, w1Frequencies.getOrDefault(c, 0) + 1);
 		}
-		Map<Character, Integer> w2Frequencies = new HashMap<>();
-		for (Character c : w2.toCharArray()) {
-			w2Frequencies.put(c, w2Frequencies.getOrDefault(c, 0) + 1);
-		}
 
-		for(Map.Entry<Character, Integer> e :w2Frequencies.entrySet()){
-			if(!w1Frequencies.containsKey(e.getKey())) return false;
-			if(w1Frequencies.get(e.getKey())<e.getValue()) return false;
+
+		for(Character c :w2.toCharArray()){
+			w1Frequencies.put(c,w1Frequencies.getOrDefault(c,0)-1);
+			if(w1Frequencies.get(c)<0) return false;
 		}
 
 
